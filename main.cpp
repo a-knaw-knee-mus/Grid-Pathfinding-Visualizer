01bf93f1df3740c9f1f9082a179cb97bba81bb73
Muhammad Mehdi Ali <64282992+a-knaw-knee-mus@users.noreply.github.com>
2024-05-06 12:43:12 -0400

    select start/end node; start search algo button

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ enum cellState {
     Clear,
     Wall,
     Start,
-    Stop,
+    End,
 };
 
 int main()
@@ -28,26 +28,84 @@ int main()
 
     bool isLeftMousePressed = false;
     bool isRightMousePressed = false;
+    bool sKeyPressed = false;
+    bool eKeyPressed = false;
+
+    int startCellIdx[2] = {-1, -1}; // coords for start cell
+    int endCellIdx[2] = {-1, -1};   // coorsd for end cell
 
     while (window.isOpen()) {
-        Event event;
+        Event event{};
         while (window.pollEvent(event)) {
             if (event.type == Event::Closed) {
                 window.close();
             }
-            else if (event.type == sf::Event::MouseButtonPressed) {
-                if (event.mouseButton.button == sf::Mouse::Left) {
+
+            // run search algorithm
+            else if (event.type == Event::KeyPressed && event.key.code == Keyboard::R) {
+                if (startCellIdx[0] == -1 || startCellIdx[1] == -1) continue;
+                if (endCellIdx[0] == -1 || endCellIdx[1] == -1) continue;
+
+                cout << "run search" << endl;
+            }
+
+            // set start cell
+            else if (event.type == Event::KeyPressed && event.key.code == Keyboard::S) {
+                sKeyPressed = true;
+            }
+            else if (event.type == Event::KeyReleased && event.key.code == Keyboard::S) {
+                sKeyPressed = false;
+            }
+            else if (sKeyPressed && event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left) {
+                Vector2i mousePosition = Mouse::getPosition(window);
+                int cellX = mousePosition.x / cellSize;
+                int cellY = mousePosition.y / cellSize;
+                if (cellX >= 0 && cellX < gridSize && cellY >= 0 && cellY < gridSize) {
+                    cellStates[cellX][cellY] = Start;
+                }
+                if (startCellIdx[0] != -1 && startCellIdx[1] != -1) { // remove last selected start cell
+                    cellStates[startCellIdx[0]][startCellIdx[1]] = Clear;
+                }
+                // save new location for start cell
+                startCellIdx[0] = cellX;
+                startCellIdx[1] = cellY;
+            }
+
+            // set end cell
+            else if (event.type == Event::KeyPressed && event.key.code == Keyboard::E) {
+                eKeyPressed = true;
+            }
+            else if (event.type == Event::KeyReleased && event.key.code == Keyboard::E) {
+                eKeyPressed = false;
+            }
+            else if (eKeyPressed && event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left) {
+                Vector2i mousePosition = Mouse::getPosition(window);
+                int cellX = mousePosition.x / cellSize;
+                int cellY = mousePosition.y / cellSize;
+                if (cellX >= 0 && cellX < gridSize && cellY >= 0 && cellY < gridSize) {
+                    cellStates[cellX][cellY] = End;
+                }
+                if (endCellIdx[0] != -1 && endCellIdx[1] != -1) { // remove last selected end cell
+                    cellStates[endCellIdx[0]][endCellIdx[1]] = Clear;
+                }
+                // save new location for end cell
+                endCellIdx[0] = cellX;
+                endCellIdx[1] = cellY;
+            }
+
+            else if (event.type == Event::MouseButtonPressed) {
+                if (event.mouseButton.button == Mouse::Left) {
                     isLeftMousePressed = true;
                 }
-                else if (event.mouseButton.button == sf::Mouse::Right) {
+                else if (event.mouseButton.button == Mouse::Right) {
                     isRightMousePressed = true;
                 }
             }
-            else if (event.type == sf::Event::MouseButtonReleased) {
-                if (event.mouseButton.button == sf::Mouse::Left) {
+            else if (event.type == Event::MouseButtonReleased) {
+                if (event.mouseButton.button == Mouse::Left) {
                     isLeftMousePressed = false;
                 }
-                else if (event.mouseButton.button == sf::Mouse::Right) {
+                else if (event.mouseButton.button == Mouse::Right) {
                     isRightMousePressed = false;
                 }
             }
@@ -69,7 +127,7 @@ int main()
             }
         }
 
-        window.clear(Color::Black);
+        window.clear();
 
         // Draw the grid of cells
         for (int x = 0; x < gridSize; ++x) {
@@ -77,6 +135,10 @@ int main()
                 cell.setPosition(x * cellSize, y * cellSize);
                 if (cellStates[x][y] == Wall) {
                     cell.setFillColor(Color::Black);
+                } else if (cellStates[x][y] == Start) {
+                    cell.setFillColor(Color::Red);
+                } else if (cellStates[x][y] == End) {
+                    cell.setFillColor(Color::Green);
                 } else {
                     cell.setFillColor(Color::White);
                 }
